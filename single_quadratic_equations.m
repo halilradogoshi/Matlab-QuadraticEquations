316e9792d45765446fc460ae3580e75ae5add8d3
Halil Radogoshi <halilradogoshi@gmail.com>
2015-06-01 22:22:42 +0200

    Re-created the project

---
+++ single_quadratic_equations.m
@@ -0,0 +1,9 @@
+%Solving single quadratic equations
+syms(x);
+solve (2*x^2-3*x-7==0)
+
+%Svaret blir 65^(1/2)/4 + 3/4
+%Svaret blir 3/4 - 65^(1/2)/4
+
+real(65^(1/2)/4 + 3/4)
+real(3/4 - 65^(1/2)/4)
